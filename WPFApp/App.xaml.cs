25cb0d51c335b444fd1e891773aefbc512c4c1af
Vũ Quang Nguyên <vqneabung@gmail.com>
2024-11-06 22:27:59 +0700

    Update App.xaml.cs

--- WPFApp/App.xaml.cs
+++ WPFApp/App.xaml.cs
@@ -91,7 +91,6 @@ namespace WPFApp
             services.AddScoped<BlogNewsManagementWindow>();
             services.AddScoped<UpdateBlogNews>();
             services.AddScoped<MainWindow>();
-            services.AddScoped<AssignmentManagementWindow>();
         }
     }
 
